d6251769bc2045af13065c5891902d506c574418
Javier <131615003+javier-estrada-amat@users.noreply.github.com>
2025-05-06 22:21:45 +0200

    Refactor: mejorar la estructura de la página de inicio y agregar tarjetas de navegación

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { environment } from 'environments/environment';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from "../common/header/header.component";
+import { MatIcon } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatIcon],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
