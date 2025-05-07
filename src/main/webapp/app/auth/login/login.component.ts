af8e2804a768c96f4ad8795c97782fca46480415
Javier <131615003+javier-estrada-amat@users.noreply.github.com>
2025-05-07 22:34:03 +0200

    feat: Add container styling to various components for improved layout
    - Wrapped content in a container with margin and padding for better spacing in:
      - configuracionautenticacion-edit.component.html
      - configuracionautenticacion-list.component.html
      - empresas-add.component.html
      - empresas-edit.component.html
      - empresas-list.component.html
      - fichajes-add.component.html
      - fichajes-control.component.html
      - fichajes-edit.component.html
      - fichajes-list.component.html
      - historialactividad-add.component.html
      - historialactividad-edit.component.html
      - historialactividad-list.component.html
      - home.component.html
      - horasextras-add.component.html
      - horasextras-approve.component.html
      - horasextras-edit.component.html
      - horasextras-list.component.html
      - registrocambioscontrasenia-add.component.html
      - registrocambioscontrasenia-edit.component.html
      - registrocambioscontrasenia-list.component.html
      - roles-edit.component.html
      - roles-list.component.html
      - usuarios-add.component.html
      - usuarios-edit.component.html
      - usuarios-list.component.html

--- src/main/webapp/app/auth/login/login.component.ts
+++ src/main/webapp/app/auth/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,NgOptimizedImage],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
